e002c3136761112a242748bfcaef994375af7520
Nguyễn Gia Hậu <nguyengiahau2004@gmail.com>
2026-09-12 08:47:36 +0700

    update

--- coderank-api/src/providers/integrations/bullmq.provider.ts
+++ coderank-api/src/providers/integrations/bullmq.provider.ts
@@ -7,8 +7,8 @@ import { ExpressAdapter } from '@bull-board/express';
   imports: [
     BullModule.forRoot({
       connection: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST || 'localhost',
+        port: parseInt(process.env.REDIS_PORT || '6379', 10),
       },
     }),
     BullBoardModule.forRoot({
--- coderank-client/src/environments/environment.ts
+++ coderank-client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: '/api',
   production: true,
 };
